922f8fa2c91afc890df83624867853a527f604b7
Colin Mitchell <colin@muffinlabs.com>
2019-01-11 17:10:55 -0500

    Improve fullscreen check on Windows
    Only return true for windows which are fullscreen but don't have any
    chrome around the edges -- ie, maximized windows that aren't
    fullscreen

--- Fullscreen.cc
+++ Fullscreen.cc
@@ -6,6 +6,10 @@
 #define NOT_FULLSCREEN_KEY "Menubar"
 #endif
 
+#ifdef IS_WINDOWS
+#include <Windows.h>
+#endif
+
 NAN_METHOD(isFullscreen);
 
 // Example with node ObjectWrap
@@ -57,6 +61,58 @@ NAN_METHOD(MyObject::New) {
 
 using v8::FunctionTemplate;
 
+#ifdef IS_WINDOWS
+// NOTES:
+//
+// https://docs.microsoft.com/en-us/windows/desktop/winmsg/window-styles
+// https://chromium.googlesource.com/chromium/src/+/3710d00089e1b6d64a92a50a060b9581abd33fcd/chrome/browser/fullscreen_win.cc
+// http://www.jasinskionline.com/windowsapi/ref/g/getwindowlong.html
+// WS_MAXIMIZEBOX?
+// https://docs.microsoft.com/en-us/windows/desktop/winmsg/window-styles 
+
+
+/**
+ * this code is lightly modified from the chromium source code at:
+ * https://chromium.googlesource.com/chromium/src/+/3710d00089e1b6d64a92a50a060b9581abd33fcd/chrome/browser/fullscreen_win.cc
+ */
+bool IsFullScreenWindowMode() {
+  // Get the foreground window which the user is currently working on.
+  HWND wnd = ::GetForegroundWindow();
+  if (!wnd) {
+    return false;
+  }
+  // Get the monitor where the window is located.
+  RECT wnd_rect;
+  if (!::GetWindowRect(wnd, &wnd_rect)) {
+    return false;
+  }
+  HMONITOR monitor = ::MonitorFromRect(&wnd_rect, MONITOR_DEFAULTTONULL);
+  if (!monitor) {
+    return false;
+  }
+
+  MONITORINFO monitor_info = { sizeof(monitor_info) };
+  if (!::GetMonitorInfo(monitor, &monitor_info)) {
+    return false;
+  }
+
+  // The window should be at least as large as the monitor.
+  if (!::IntersectRect(&wnd_rect, &wnd_rect, &monitor_info.rcMonitor)) {
+    return false;
+  }
+
+  if (!::EqualRect(&wnd_rect, &monitor_info.rcMonitor)) {
+    return false;
+  }
+
+  // At last, the window style should not have WS_DLGFRAME and WS_THICKFRAME and
+  // its extended style should not have WS_EX_WINDOWEDGE and WS_EX_TOOLWINDOW.
+  LONG style = ::GetWindowLong(wnd, GWL_STYLE);
+  LONG ext_style = ::GetWindowLong(wnd, GWL_EXSTYLE);
+  return !((style & (WS_DLGFRAME | WS_THICKFRAME)) ||
+           (ext_style & (WS_EX_WINDOWEDGE | WS_EX_TOOLWINDOW)));
+}
+#endif
 
 //bool Method(napi_env env, napi_callback_info info) {
 NAN_METHOD(isFullscreen) {
@@ -99,24 +155,7 @@ NAN_METHOD(isFullscreen) {
   #endif
 
   #ifdef IS_WINDOWS
-	// v8::Local<v8::Function> cons = Nan::New(constructor);
-	// const int argc = 1;
-	HWND dskWin = GetDesktopWindow();
-	HWND fgWin = GetForegroundWindow();
-	bool result = false;
-
-	RECT appBounds;
-	RECT rc;
-
-	if ( fgWin != dskWin && fgWin != GetShellWindow() ) {
-		GetWindowRect(dskWin, &rc);
-		GetWindowRect(fgWin, &appBounds);
-
-		if ( appBounds.left <= rc.left && appBounds.top <= rc.top && appBounds.right >= rc.right && appBounds.bottom >= rc.bottom ) {
-			result = true;
-		}
-	}
-	
+	bool result = IsFullScreenWindowMode();
   info.GetReturnValue().Set(result);
   #endif
 
